7a1b45eac9be1c358d3a3d2971afd85bc3a3a895
Kyrylo <kyryloyunytsky@gmail.com>
2024-10-28 22:17:41 +0200

    Integrate map

--- client/src/views/Journey.jsx
+++ client/src/views/Journey.jsx
@@ -2,13 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { deleteJourney, getJourney } from '../api';
 import ConfirmationModal from '../components/ConfirmationModal';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+
+const MapView = ({ center }) => {
+  const map = useMap();
+  useEffect(() => {
+    if (center) {
+      map.setView(center, map.getZoom());
+    }
+  }, [center, map]);
+
+  return null; // This component does not render anything visible
+};
 
 const Journey = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [journey, setJourney] = useState(null);
   const [showModal, setShowModal] = useState(false);
-
+  const [selectedLocationId, setSelectedLocationId] = useState(0);
+  
   useEffect(() => {
     const fetchJourney = async () => {
       try {
@@ -44,24 +58,93 @@ const Journey = () => {
   }
 
   return (
-    <div className="container mt-5 pb-4">
-      <article className="text-center">
+    <div className="container mt-5 pb-5">
+      <article>
+        {/* Title */}
+        <h1 className="display-4">{journey.title}</h1>
+
+        {/* Date */}
+        <p className="text-muted">
+          <small>
+            {new Date(journey.startDate).toLocaleDateString()} –{" "}
+            {new Date(journey.endDate).toLocaleDateString()}
+          </small>
+        </p>
+
+        {/* Image */}
         <img
           src={journey.image.url}
           className="img-fluid mb-4"
           alt={journey.title}
-          style={{ maxHeight: '380px', width: 'auto' }}
+          style={{ maxHeight: "380px", width: "auto" }}
         />
-        <h1 className="display-4">{journey.title}</h1>
-        <p className="text-muted">
-          <small>{new Date(journey.startDate).toLocaleDateString()} – {new Date(journey.endDate).toLocaleDateString()}</small>
-        </p>
-        <p className="lead">{journey.description}</p>
-        <div>
-          <Link
-            to="edit"
-            className="btn btn-primary me-2"
+
+        {/* Description */}
+        <p className="mb-4 text-start">{journey.description}</p>
+
+        {/* Locations */}
+        {journey.locations.length > 0 && (
+          <>
+            <h2>Locations</h2>
+            {journey.locations.map((location, index) => (
+              <button
+                onClick={() => {
+                  setSelectedLocationId(index);
+                }}
+                className={
+                  selectedLocationId === index
+                    ? "mb-1 btn p-0 fw-medium text-primary"
+                    : "mb-1 btn p-0"
+                }
+                key={index}
+                style={{ border: "none" }}
+              >
+                {location.name} {location.coordinates.lat}{" "}
+                {location.coordinates.lng}
+              </button>
+            ))}
+          </>
+        )}
+
+        {journey.locations.length > 0 && (
+          <MapContainer
+            center={[
+              journey.locations[selectedLocationId]?.coordinates.lat || 51.505,
+              journey.locations[selectedLocationId]?.coordinates.lng || -0.09,
+            ]}
+            zoom={13}
+            style={{
+              height: "460px",
+              maxWidth: "980px",
+              margin: "2em 0 4em 0",
+            }}
           >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <MapView
+              center={[
+                journey.locations[selectedLocationId]?.coordinates.lat || 51.505,
+                journey.locations[selectedLocationId]?.coordinates.lng || -0.09,
+              ]}
+            />
+            {journey.locations.map((location, index) => (
+              <Marker
+                key={index}
+                position={[location.coordinates.lat, location.coordinates.lng]}
+              >
+                <Popup>{location.name}</Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        )}
+
+        <hr className="w-75" />
+
+        {/* Buttons */}
+        <div>
+          <Link to="edit" className="btn btn-primary me-2">
             Edit
           </Link>
           <button className="btn btn-danger" onClick={handleDeleteClick}>
